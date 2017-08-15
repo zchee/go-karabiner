5000168ee0624e2029da481905dade1a382c25aa
Koichi Shiraishi <zchee.io@gmail.com>
2017-08-16 03:10:18 +0900

    types: move type related code to types package
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- types/keycode.go
+++ types/keycode.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package karabiner
+package types
 
 // KeyCode represents a karabiner keycode.
 // This conevert table based by:
